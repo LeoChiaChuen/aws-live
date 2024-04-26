6f16d4ace6cb9b51f8f1ff2d39b168f6f7685d56
LeoChiaChuen <167172144+LeoChiaChuen@users.noreply.github.com>
2024-04-26 22:00:31 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cboaw82g8yfo.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3523"
+customdb = "employee"
+custombucket = "leochiachuen-employee"
+customregion = "us-east-1"
+
